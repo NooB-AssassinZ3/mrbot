d1701397ba868fbb95de4b37ed3f7a3f117d80f5
User <user@noobstuff.com>
2017-04-10 18:30:36 -0500

    init comit

---
+++ bot.js
@@ -0,0 +1,63 @@
+const Discord = require("discord.js");
+const Reload = require("self-reload-json");
+const console = require("console");
+const nodeliquidsoap = require("/usr/local/lib/node_modules/liquidsoap/lib/node/liquidsoap.js");
+
+const prefix = ';';
+const token = "Mjg5NTY3ODk0ODI2Nzc4NjI1.C6ORkg.j-kbV3kvSum1D5wkmwPv8nZQnk0";
+
+var adminList = new Reload("./data/admins.json");
+var bot = new Discord.Client();
+
+
+bot.login(token);
+
+bot.on("message", msg => {
+    if (msg.content.startsWith(prefix + "userinfo")) {
+        let args = processArgs(msg.content);
+        msg.channel.sendMessage(
+            "Role ID: " + msg.member.highestRole.id +
+            "\nUser ID: " + msg.member.id +
+            "\nUser Name: " + msg.member.username + "#" + msg.member.discriminator +
+            "\nAdmin: " + checkPermissions(msg.member.username));
+        console.log("userinfo done by: " + msg.member.username +
+            ", args: " + args);
+    } else if (msg.content.startsWith(prefix + "off") && checkPermissions(msg.author.username)) {
+        msg.channel.sendMessage("Shutting Down...");
+        bot.destroy();
+    } else if (msg.content.startsWith(prefix + "listadmins")) {
+        for (var value in adminList) {
+            msg.channel.sendMessage(value);
+        }
+    } else if (msg.content.startsWith(prefix + "checkadmin")) {
+        msg.channel.sendMessage(checkPermissions(msg.author.username))
+    } else if (msg.content.startsWith(prefix + "reboot") && checkPermissions(msg.author.username)) {
+        reboot()
+    }
+});
+setInterval(update, 10000);
+
+function update() {
+
+}
+
+function reboot() {
+    bot.destroy();
+    bot.login(token);
+}
+
+function checkPermissions(userInput) {
+    for (var value in adminList) {
+        if (userInput == value) {
+            return true;
+            break;
+        } else {
+            return false;
+        }
+    }
+}
+
+function processArgs(args) {
+    args = args.split(";");
+    return args;
+}
